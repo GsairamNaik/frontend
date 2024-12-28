dea3892b725a758014e38863a29274ebcba8d510
gsairam <gugulothusairam5@gmail.com>
2024-12-28 23:23:02 +0530

    my first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero container' >
+     <div className="hero-text"></div>
+     <h1>We Ensure better education for a better world</h1>
+     <p>Our cutting-egge curriculum is designed to emopwer students with the 
+        knowlege, skills, & experiences needed to excel in the dynamic field of education
+     </p>
+     <button className='btn'>Explore More</button>
+    </div>
+  )
+}
+
+export default Hero
